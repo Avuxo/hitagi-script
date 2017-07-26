442cb1cb701109cf43d84e44e70d5a68f802a877
Ben <flightsimstorage@gmail.com>
2017-07-25 21:02:20 -0400

    fix batch file?

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-B@echo off
+@echo off
 cls
 @echo:
 echo     ,,,                 ;;                      ;, 
@@ -15,10 +15,10 @@ echo     ;;;;;;;             ;;    ;;;;;,,      \`;;;;;,,
 echo ~~Hitagi-Script Windows Edition~~
 echo Please ensure that you have installed NodeJS 7.x
 echo https://nodejs.org/en/download/current/
-REM call npm install
+call npm install
 
 set /p name="Safebooru tags (name or additional queries)? "
 
 echo Downloading art...
-call node danbooru.js %name%
+call node danbooru-win.js %name%
 pause
